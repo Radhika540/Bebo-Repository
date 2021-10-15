f829c25b2fe06fbfc6ebddcd9660c804d5e7566e
Radhika540 <83178989+Radhika540@users.noreply.github.com>
2021-10-15 13:01:03 +0530

    Delete employeeManagement directory

--- employeeManagement/src/main/java/com/employee/management/employeeManagement/dao/RecordsDao.java
+++
@@ -1,9 +0,0 @@
-package com.employee.management.employeeManagement.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.employee.management.employeeManagement.enteties.Records;
-
-public interface RecordsDao extends JpaRepository<Records, String> {
-
-}
